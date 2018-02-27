c5928dbf37622b3b48ff1d05d63758ae228b935c
turpure <p812816627@163.com>
2018-02-27 15:35:42 +0800

    get token

---
+++ common/token.py
@@ -0,0 +1,26 @@
+#! usr/bin/python
+# coding:utf8
+
+from db import MsSQL
+
+
+connection = MsSQL()
+
+
+def get_token(alias_name):
+    """
+    get token from table
+    :return:string
+    """
+    sql = "select AccessToken from S_JoomSyncInfo where AliasName=%s"
+
+    with connection as con:
+        cur = con.cursor(as_dict=True)
+        cur.execute(sql, (alias_name,))
+        ret = cur.fetchone()
+        if isinstance(ret, dict):
+            return ret['AccessToken']
+
+
+if __name__ ==  "__main__":
+    print get_token('Joom01-YHBaby')
--- src/get_products.py
+++ src/get_products.py
@@ -1,4 +1,9 @@
 #! usr/bin/python
 # coding:utf8
 
+import requests
+from common.db import MsSQL
 
+
+def get_all_products():
+    pass
