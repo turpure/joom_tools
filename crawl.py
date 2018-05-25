d4eb66d0240b45472072bdb734e6a7b412652ae7
turpure <p812816627@163.com>
2018-05-25 15:09:11 +0800

    rename

---
+++ crawl.py
@@ -0,0 +1,8 @@
+#! usr/bin/python
+# coding:utf8
+
+from src import crawler
+
+worker = crawler.Crawler()
+worker.run()
+
--- main.py
+++
@@ -1,7 +0,0 @@
-#! usr/bin/python
-# coding:utf8
-
-from src import fetch_products as worker
-
-worker.crawler()
-
